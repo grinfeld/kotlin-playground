b6c78948af0e2a23ab53de4c07a8ae7ced58fd8b
Grinfeld Mikhail <grinfeld@gmail.com>
2019-11-15 19:02:36 +0200

    playing

--- src/main/kotlin/com/mikerusoft/kotlin/HelloWorld.kt
+++ src/main/kotlin/com/mikerusoft/kotlin/HelloWorld.kt
@@ -1,8 +1,38 @@
 package com.mikerusoft.kotlin
 
+import java.lang.StringBuilder
+
 fun main() {
     println("Hello World")
     val greet = "hello"
     println(greet::class)
     println(greet::class.java)
+
+    var factor = 2
+
+    fun doubleIt(n : Int) = n * factor
+    println(doubleIt(2))
+    factor = 0
+    println(doubleIt(2))
+
+    val message = StringBuilder("hello ");
+    message.append("There")
+    println(message)
+
+    println("hi" == "hi")
+    println("hi" == "Hi")
+    println(null == "hi")
+    println("hi" == null)
+    println(null == null)
+
+
+    val price = 12.25
+    val taxRate = 0.08
+
+    val output = "The amount $price after tax comes to $${price * (1 + taxRate)}"
+    val disclaimer = "The amount is in US$, that's right in \$only"
+
+    println(output)
+    println(disclaimer)
 }
+
